117d7863fdc8311142a39109c334a96005c4734e
Mizuki Sango <msageha@yahoo.co.jp>
2018-02-19 01:24:24 +0900

    add

--- anaphora/src/lstm/model.py
+++ anaphora/src/lstm/model.py
@@ -6,19 +6,20 @@ from chainer import reporter
 import chainer.links as L
 import chainer.functions as F
 
-def convert_seq(batch, device=0, with_label=True):
+def convert_seq(batch, device=None, with_label=True):
     def to_device_batch(batch):
-        # if device is None:
-        #     return batch
-        # elif device < 0:
-        #     return [chainer.dataset.to_device(device, x) for x in batch]
-        # else:
-        xp = cuda.cupy.get_array_module(*batch)
-        concat = xp.concatenate(batch, axis=0)
-        sections = np.cumsum([x.shape[0] for x in batch[:-1]], dtype='i')
-        concat_dev = chainer.dataset.to_device(device, concat)
-        batch_dev = cuda.cupy.split(concat_dev, sections)
-        return batch_dev
+        return [chainer.dataset.to_device(device, x) for x in batch]
+        if device is None:
+            return batch
+        elif device < 0:
+            return [chainer.dataset.to_device(device, x) for x in batch]
+        else:
+            xp = cuda.cupy.get_array_module(*batch)
+            concat = xp.concatenate(batch, axis=0)
+            sections = np.cumsum([x.shape[0] for x in batch[:-1]], dtype='i')
+            concat_dev = chainer.dataset.to_device(device, concat)
+            batch_dev = cuda.cupy.split(concat_dev, sections)
+            return batch_dev
 
     if with_label:
         return {'xs': to_device_batch([x for x, _ in batch]),
