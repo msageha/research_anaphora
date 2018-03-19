0cbc1d2830a7da49c317338ea3b68e551258213f
Mizuki Sango <msageha@yahoo.co.jp>
2018-03-19 16:02:33 +0900

    add

--- basic_processing/diff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for file in *; do
-   echo "${file}";
-   diff "${file}"  "../../extract_text/OC/${file}";
-done
--- basic_processing/to_utf8.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for file in *; do
-   echo "${file}";
-   nkf -w --overwrite "${file}";
-done
