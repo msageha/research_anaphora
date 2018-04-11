57991e61efd53e98e2cc35c86b64376493efcd9d
Mizuki Sango <msageha@yahoo.co.jp>
2018-04-11 22:36:41 +0900

    first commit

--- src/lstm/original_simultaneously/model.py
+++ src/lstm/original_simultaneously/model.py
@@ -16,13 +16,6 @@ def convert_seq(batch, device=None, with_label=True):
         # else device < 0:
         else:
             return [chainer.dataset.to_device(device, x) for x in batch]
-        # else:
-        #     xp = cuda.cupy.get_array_module(*batch)
-        #     concat = xp.concatenate(batch, axis=0)
-        #     sections = np.cumsum([x.shape[0] for x in batch[:-1]], dtype='i')
-        #     concat_dev = chainer.dataset.to_device(device, concat)
-        #     batch_dev = cuda.cupy.split(concat_dev, sections)
-        #     return batch_dev
     if with_label:
         return {'xs': to_device_batch([x for x, _ in batch]),
                 'ys': to_device_batch([y for _, y in batch])}
@@ -49,16 +42,30 @@ class BiLSTMBase(Chain):
         for pred_y, y in zip(pred_ys, ys):
             _loss = F.softmax_cross_entropy(pred_y, y)
             loss += _loss
+
         reporter.report({'loss': loss.data}, self)
+        accuracy_ga = .0
+        accuracy_o = .0
+        accuracy_ni = .0
+        accuracy_all = .0
 
-        accuracy = .0
-
-        ipdb.set_trace()
-        pred_ys = [pred_y.data.argmax(axis=0) for pred_y in pred_ys]
+        pred_ys = [pred_y.data.argmax(axis=1) for pred_y in pred_ys]
         for pred_y, y in zip(pred_ys, ys):
-            if y == pred_y:
-                accuracy += 1/len(ys)
-        reporter.report({'accuracy': accuracy}, self)
+            if y[0] == pred_y[0]:
+                accuracy_ga += 1/len(ys)
+                accuracy_all += 1/len(ys)/3
+            if y[1] == pred_y[1]:
+                accuracy_o += 1/len(ys)
+                accuracy_all += 1/len(ys)/3
+            if y[2] == pred_y[2]:
+                accuracy_ni += 1/len(ys)
+                accuracy_all += 1/len(ys)/3
+
+        reporter.report({'accuracy_ga': accuracy_ga}, self)
+        reporter.report({'accuracy_o': accuracy_o}, self)
+        reporter.report({'accuracy_ni': accuracy_ni}, self)
+        reporter.report({'accuracy_all': accuracy_all}, self)
+
         return loss
 
     def traverse(self, xs):
