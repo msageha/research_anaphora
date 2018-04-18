393c6525262b2732c8d1a44cfbdb44e91ba462dc
Mizuki Sango <msageha@yahoo.co.jp>
2018-04-19 01:19:48 +0900

    first commit

--- src/lstm/original_simultaneously/train.py
+++ src/lstm/original_simultaneously/train.py
@@ -142,6 +142,38 @@ def main(train_test_ratio=0.8):
         test_data  = tuple_dataset.TupleDataset(union_test_x, union_test_y)
         training(train_data, test_data, 'union_pert_{0}'.format(N), today, args)
 
+def out_domain(train_test_ratio=0.8):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_layers', '-n', type=int, default=2)
+    parser.add_argument('--dropout', '-d', type=float, default=0.3)
+    parser.add_argument('--batchsize', '-b', type=int, default=30)
+    parser.add_argument('--epoch', '-e', type=int, default=10)
+    parser.add_argument('--gpu', '-g', type=int, default=0)
+    parser.add_argument('--out', '-o', default='out_domain', help='Directory to output the result')
+    parser.add_argument('--is_short', action='store_true')
+    args = parser.parse_args()
+
+    today = str(datetime.datetime.today())[:-16]
+    dataset_dict = load_dataset(args.is_short)
+    print('start data load out_domain')
+    for out_domain in domain_dict:
+        union_train_x = []
+        union_test_x = []
+        union_train_y = []
+        union_test_y = []
+        for domain in domain_dict:
+            if out_domain == domain:
+                continue
+            size = math.ceil(len(dataset_dict['{0}_x'.format(domain)])*train_test_ratio)
+            union_train_x += dataset_dict['{0}_x'.format(domain)][:size]
+            union_test_x += dataset_dict['{0}_x'.format(domain)][size:]
+            union_train_y += dataset_dict['{0}_y'.format(domain)][:size]
+            union_test_y += dataset_dict['{0}_y'.format(domain)][size:]
+        print('out domain {0}\tdata_size {1}'.format(out_domain, len(union_train_x)))
+        train_data = tuple_dataset.TupleDataset(union_train_x, union_train_y)
+        test_data  = tuple_dataset.TupleDataset(union_test_x, union_test_y)
+        training(train_data, test_data, 'out-{0}'.format(out_domain), today, args)
+
 if __name__ == '__main__':
     '''
     パラメータ
@@ -152,4 +184,4 @@ if __name__ == '__main__':
     epoch
     optimizer(adam)
     '''
-    main()
+    out_domain()
