55587d21d8f24ca24db8752956f09c367b97098e
Mizuki Sango <msageha@yahoo.co.jp>
2018-04-25 13:34:46 +0900

    edit

--- src/lstm/original1/model.py
+++ src/lstm/original1/model.py
@@ -13,16 +13,15 @@ def convert_seq(batch, device=None, with_label=True):
     def to_device_batch(batch):
         if device is None:
             return batch
-        # else device < 0:
-        else:
+        else device < 0:
             return [chainer.dataset.to_device(device, x) for x in batch]
-        # else:
-        #     xp = cuda.cupy.get_array_module(*batch)
-        #     concat = xp.concatenate(batch, axis=0)
-        #     sections = np.cumsum([x.shape[0] for x in batch[:-1]], dtype='i')
-        #     concat_dev = chainer.dataset.to_device(device, concat)
-        #     batch_dev = cuda.cupy.split(concat_dev, sections)
-        #     return batch_dev
+        else:
+            xp = cuda.cupy.get_array_module(*batch)
+            concat = xp.concatenate(batch, axis=0)
+            sections = np.cumsum([x.shape[0] for x in batch[:-1]], dtype='i')
+            concat_dev = chainer.dataset.to_device(device, concat)
+            batch_dev = cuda.cupy.split(concat_dev, sections)
+            return batch_dev
     if with_label:
         return {'xs': to_device_batch([x for x, _ in batch]),
                 'ys': to_device_batch([y for _, y in batch])}
@@ -33,8 +32,6 @@ class BiLSTMBase(Chain):
     def __init__(self, input_size, n_labels, n_layers=1, dropout=0.5):
         super(BiLSTMBase, self).__init__()
         with self.init_scope():
-      # self.f_lstm = L.LSTM(None, feature_size, dropout)
-      # self.b_lstm = L.LSTM(None, feature_size, dropout)
             self.nstep_bilstm = L.NStepBiLSTM(n_layers=n_labels, in_size=input_size, out_size=input_size, dropout=dropout)
             self.l1 = L.Linear(input_size*2, n_labels)
 
