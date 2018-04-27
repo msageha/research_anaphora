428f8cfd10e8b31e66b103e3e1db01a4eb33b8df
Mizuki Sango <msageha@yahoo.co.jp>
2018-04-27 17:38:55 +0900

    edit

--- src/lstm/original1/model.py
+++ src/lstm/original1/model.py
@@ -13,7 +13,7 @@ def convert_seq(batch, device=None, with_label=True):
     def to_device_batch(batch):
         if device is None:
             return batch
-        else:
+        elif device < 0:
             return [chainer.dataset.to_device(device, x) for x in batch]
         else:
             xp = cuda.cupy.get_array_module(*batch)
