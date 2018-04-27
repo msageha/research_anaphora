503424458e4a0fba67f62c8aa32682a723011c11
Mizuki Sango <msageha@yahoo.co.jp>
2018-04-27 17:38:35 +0900

    edit

--- src/lstm/original1/model.py
+++ src/lstm/original1/model.py
@@ -15,13 +15,13 @@ def convert_seq(batch, device=None, with_label=True):
             return batch
         else:
             return [chainer.dataset.to_device(device, x) for x in batch]
-        # else:
-        #     xp = cuda.cupy.get_array_module(*batch)
-        #     concat = xp.concatenate(batch, axis=0)
-        #     sections = np.cumsum([x.shape[0] for x in batch[:-1]], dtype='i')
-        #     concat_dev = chainer.dataset.to_device(device, concat)
-        #     batch_dev = cuda.cupy.split(concat_dev, sections)
-        #     return batch_dev
+        else:
+            xp = cuda.cupy.get_array_module(*batch)
+            concat = xp.concatenate(batch, axis=0)
+            sections = np.cumsum([x.shape[0] for x in batch[:-1]], dtype='i')
+            concat_dev = chainer.dataset.to_device(device, concat)
+            batch_dev = cuda.cupy.split(concat_dev, sections)
+            return batch_dev
     if with_label:
         return {'xs': to_device_batch([x for x, _ in batch]),
                 'ys': to_device_batch([y for _, y in batch])}
