f7d9e77710952275e232d6467de89c6eab84e52d
Mizuki Sango <msageha@yahoo.co.jp>
2018-05-01 10:47:48 +0900

    add

--- src/neural_net/baseline/train.py
+++ src/neural_net/baseline/train.py
@@ -9,6 +9,7 @@ from collections import OrderedDict
 import matplotlib
 matplotlib.use('Agg')
 
+import pandas as pd
 import numpy as np
 import chainer
 from chainer import cuda
@@ -29,18 +30,35 @@ def set_random_seed(seed):
     # set Chainer(CuPy) random seed
     cuda.cupy.random.seed(seed)
 
-def load_dataset(dataset_path='./dataset'):
+def load_dataset(df_path):
     dataset_dict = {}
     for domain in domain_dict:
         print('start data load domain-{0}'.format(domain))
-        dataset = np.load('{0}/{1}.npz'.format(dataset_path, domain))
-        x_dataset = list(dataset['x'])
-        y_ga_dataset = list(dataset['y_ga'])
-        y_ga_dep_tag_dataset = list(dataset['y_ga_dep_tag'])
-        y_o_dataset = list(dataset['y_o'])
-        y_o_dep_tag_dataset = list(dataset['y_o_dep_tag'])
-        y_ni_dataset = list(dataset['y_ni'])
-        y_ni_dep_tag_dataset = list(dataset['y_ni_dep_tag'])
+        with open('{0}/dataframe_list_{1}.pickle'.format(df_path, domain), 'rb') as f:
+            df_list = pickle.load(f)
+        x_dataset = []
+        y_ga_dataset = []
+        y_ga_dep_tag_dataset = []
+        y_o_dataset = []
+        y_o_dep_tag_dataset = []
+        y_ni_dataset = []
+        y_ni_dep_tag_dataset = []
+        for df in df_list:
+            y_ga = np.array(df['ga_case'], dtype=np.int32)
+            y_o = np.array(df['o_case'], dtype=np.int32)
+            y_ni = np.array(df['ni_case'], dtype=np.int32)
+            y_ga_dep_tag = np.array(df['ga_dep_tag'])
+            y_o_dep_tag = np.array(df['o_dep_tag'])
+            y_ni_dep_tag = np.array(df['ni_dep_tag'])
+            df = df.drop('ga_case', axis=1).drop('o_case', axis=1).drop('ni_case', axis=1).drop('ga_dep_tag', axis=1).drop('o_dep_tag', axis=1).drop('ni_dep_tag', axis=1)
+            x = np.array(df, dtype=np.float32)
+            x_dataset.append(x)
+            y_ga_dataset.append(y_ga)
+            y_ga_dep_tag_dataset.append(y_ga_dep_tag)
+            y_o_dataset.append(y_o)
+            y_o_dep_tag_dataset.append(y_o_dep_tag)
+            y_ni_dataset.append(y_ni)
+            y_ni_dep_tag_dataset.append(y_ni_dep_tag)
         dataset_dict['{0}_x'.format(domain)] = x_dataset
         dataset_dict['{0}_y_ga'.format(domain)] = y_ga_dataset
         dataset_dict['{0}_y_o'.format(domain)] = y_o_dataset
@@ -101,25 +119,25 @@ def training(train_data, test_data, domain, case, dump_path, args):
     trainer.run()
 
 def in_domain(dataset_dict, args, dump_path):
-    for domain in domain_dict:
-        size = math.ceil(len(dataset_dict['{0}_x'.format(domain)])*args.train_test_ratio)
-        train_x = dataset_dict['{0}_x'.format(domain)][:size]
-        test_x = dataset_dict['{0}_x'.format(domain)][size:]
-        train_y = dataset_dict['{0}_y_ga'.format(domain)][:size]
-        test_y = dataset_dict['{0}_y_ga'.format(domain)][size:]
-        train_data = tuple_dataset.TupleDataset(train_x, train_y)
-        test_data  = tuple_dataset.TupleDataset(test_x, test_y)
-        training(train_data, test_data, domain, 'ga', dump_path, args)
-        train_y = dataset_dict['{0}_y_o'.format(domain)][:size]
-        test_y = dataset_dict['{0}_y_o'.format(domain)][size:]
-        train_data = tuple_dataset.TupleDataset(train_x, train_y)
-        test_data  = tuple_dataset.TupleDataset(test_x, test_y)
-        training(train_data, test_data, domain, 'o', dump_path, args)
-        train_y = dataset_dict['{0}_y_ni'.format(domain)][:size]
-        test_y = dataset_dict['{0}_y_ni'.format(domain)][size:]
-        train_data = tuple_dataset.TupleDataset(train_x, train_y)
-        test_data  = tuple_dataset.TupleDataset(test_x, test_y)
-        training(train_data, test_data, domain, 'ni', dump_path, args)
+    # for domain in domain_dict:
+    #     size = math.ceil(len(dataset_dict['{0}_x'.format(domain)])*args.train_test_ratio)
+    #     train_x = dataset_dict['{0}_x'.format(domain)][:size]
+    #     test_x = dataset_dict['{0}_x'.format(domain)][size:]
+    #     train_y = dataset_dict['{0}_y_ga'.format(domain)][:size]
+    #     test_y = dataset_dict['{0}_y_ga'.format(domain)][size:]
+    #     train_data = tuple_dataset.TupleDataset(train_x, train_y)
+    #     test_data  = tuple_dataset.TupleDataset(test_x, test_y)
+    #     training(train_data, test_data, domain, 'ga', dump_path, args)
+    #     train_y = dataset_dict['{0}_y_o'.format(domain)][:size]
+    #     test_y = dataset_dict['{0}_y_o'.format(domain)][size:]
+    #     train_data = tuple_dataset.TupleDataset(train_x, train_y)
+    #     test_data  = tuple_dataset.TupleDataset(test_x, test_y)
+    #     training(train_data, test_data, domain, 'o', dump_path, args)
+    #     train_y = dataset_dict['{0}_y_ni'.format(domain)][:size]
+    #     test_y = dataset_dict['{0}_y_ni'.format(domain)][size:]
+    #     train_data = tuple_dataset.TupleDataset(train_x, train_y)
+    #     test_data  = tuple_dataset.TupleDataset(test_x, test_y)
+    #     training(train_data, test_data, domain, 'ni', dump_path, args)
     print('start data load domain-union')
     union_train_x = []
     union_test_x = []
@@ -243,9 +261,10 @@ def main():
     parser.add_argument('--gpu', '-g', type=int, default=0)
     parser.add_argument('--train_test_ratio', type=float, default=0.8)
     parser.add_argument('--seed', default=1)
+    parser.add_argument('--df_path', default='../dataframe')
     args = parser.parse_args()
 
-    dataset_dict = load_dataset()
+    dataset_dict = load_dataset(args.df_path)
     in_domain(dataset_dict, args, 'normal/dropout-{0}_batchsize-{1}'.format(args.dropout, args.batchsize))
     out_domain(dataset_dict, args, 'outdomain/dropout-{0}_batchsize-{1}'.format(args.dropout, args.batchsize))
     arrange(dataset_dict, args, 'arranged/dropout-{0}_batchsize-{1}'.format(args.dropout, args.batchsize))
