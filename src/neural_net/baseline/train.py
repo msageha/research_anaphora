a74a48fa3b62f011d8dd9ca1bd730e9ced052042
Mizuki Sango <msageha@yahoo.co.jp>
2018-05-06 22:41:34 +0900

    add

--- src/neural_net/baseline/train.py
+++ src/neural_net/baseline/train.py
@@ -264,7 +264,7 @@ def main():
     args = parser.parse_args()
 
     dataset_dict = load_dataset(args.df_path)
-    in_domain(dataset_dict, args, 'normal/dropout-{0}_batchsize-{1}'.format(args.dropout, args.batchsize))
+    # in_domain(dataset_dict, args, 'normal/dropout-{0}_batchsize-{1}'.format(args.dropout, args.batchsize))
     out_domain(dataset_dict, args, 'outdomain/dropout-{0}_batchsize-{1}'.format(args.dropout, args.batchsize))
     arrange(dataset_dict, args, 'arranged/dropout-{0}_batchsize-{1}'.format(args.dropout, args.batchsize))
 
