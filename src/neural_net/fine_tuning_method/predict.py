f68d3a418847fc0b2e6b8bb7a6eb539404770e32
Mizuki Sango <msageha@yahoo.co.jp>
2018-05-07 13:42:21 +0900

    add

--- src/neural_net/fine_tuning_method/predict.py
+++ src/neural_net/fine_tuning_method/predict.py
@@ -142,13 +142,13 @@ def main():
         union_test_ni_dep_tag += dataset_dict['{0}_y_ni_dep_tag'.format(domain)][size:]
     for case in ['ga', 'o', 'ni']:
         for model_path in load_model_path(args.dir, case):
-            if case == 'ga':
-                test_data  = tuple_dataset.TupleDataset(union_test_x, union_test_ga, union_test_ga_dep_tag)
-            elif case == 'o':
-                test_data  = tuple_dataset.TupleDataset(union_test_x, union_test_o, union_test_o_dep_tag)
-            elif case == 'ni':
-                test_data  = tuple_dataset.TupleDataset(union_test_x, union_test_ni, union_test_ni_dep_tag)
-            predict(model_path, test_data, 'union', case, args)
+            # if case == 'ga':
+            #     test_data  = tuple_dataset.TupleDataset(union_test_x, union_test_ga, union_test_ga_dep_tag)
+            # elif case == 'o':
+            #     test_data  = tuple_dataset.TupleDataset(union_test_x, union_test_o, union_test_o_dep_tag)
+            # elif case == 'ni':
+            #     test_data  = tuple_dataset.TupleDataset(union_test_x, union_test_ni, union_test_ni_dep_tag)
+            # predict(model_path, test_data, 'union', case, args)
             for domain in domain_dict:
                 size = math.ceil(len(dataset_dict['{0}_x'.format(domain)])*args.train_test_ratio)
                 test_x = dataset_dict['{0}_x'.format(domain)][size:]
