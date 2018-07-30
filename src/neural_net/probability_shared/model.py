dd9d6fb617920a32266d3e0fe5cd2ed3e4d91c73
Mizuki Sango <msageha@yahoo.co.jp>
2018-07-30 18:37:29 +0900

    first commit

--- src/neural_net/probability_shared/model.py
+++ src/neural_net/probability_shared/model.py
@@ -33,7 +33,7 @@ class BiLSTMBase(Chain):
             self.nstep_bilstm1 = L.NStepBiLSTM(n_layers=n_layers, in_size=input_size, out_size=output_size, dropout=dropout)
             self.l1 = L.Linear(input_size*2, n_labels)
             self.nstep_bilstm2 = L.NStepBiLSTM(n_layers=n_layers, in_size=n_labels, out_size=n_labels, dropout=0.0)
-            self.l2 = L.Linear(n_labels, n_labels)
+            self.l2 = L.Linear(n_labels*2, n_labels)
 
 
     def __call__(self, xs, ys):
